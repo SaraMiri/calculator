5909271fab9135c023074c348d887ffdfb45b18c
Sara Miragaya Rico <saramiragaya@gmail.com>
2018-10-23 14:55:43 +0200

    Cambiado nombre operating

--- index.js
+++ index.js
@@ -4,5 +4,21 @@ container.addEventListener("click", onClick);
 function onClick (event) {
   const operating = event.target.innerText;
   const result = document.querySelector(".result");
-  result.innerText = operating;
+  //result.innerText = operating;
+  
+  switch (operating) {
+    case '1':
+      result.innerText = operating;
+      break;
+    case '2':
+    
+      result.innerText = operating;
+      // expected output: "Mangoes and papayas are $2.79 a pound.";
+      case '3':
+      result.innerText = operating;
+      break;
+    default:
+      result.innerText = operating;
+  }
+
 }
