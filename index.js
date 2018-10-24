e9c73c969b7e7c1570bab428b10586f5629bc991
Sara Miragaya Rico <saramiragaya@gmail.com>
2018-10-24 08:27:56 +0200

    Arreglado botón clicar fuera

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 const container = document.querySelector(".container");
 container.addEventListener("click", onClick);
 
-function onClick (event) {
+function onClick(event) {
+  if (event.target.tagName === "BUTTON"){
   const operating = event.target.innerText;
   const result = document.querySelector(".result");
-  //result.innerText = operating;
+  result.innerText = operating;
   
   switch (operating) {
     case '1':
-      result.innerText = operating;
+      result.innerText <= 1;
       break;
     case '2':
-    
       result.innerText = operating;
       // expected output: "Mangoes and papayas are $2.79 a pound.";
       case '3':
-      result.innerText = operating;
+      //result.innerText = operating;
       break;
     default:
-      result.innerText = operating;
+     // result.innerText = operating;
   }
-
+}
 }
