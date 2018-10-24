10fdb77d911e5d1d69404dc0243f1d9e69b6e453
Sara Miragaya Rico <saramiragaya@gmail.com>
2018-10-24 10:19:37 +0200

    Switch listo para manejar números

--- index.js
+++ index.js
@@ -1,45 +1,42 @@
 const container = document.querySelector(".container");
 container.addEventListener("click", onClick);
 
+let num1 = "";
+let num2 = "";
+let operator = "";
+
 function onClick(event) {
-  if (event.target.tagName === "BUTTON"){
-  const operating = event.target.innerText;
-  const result = document.querySelector(".result");
-  result.innerText = operating;
+  if (event.target.tagName === "BUTTON") {
+    const operating = event.target.innerText;
+    const result = document.querySelector(".result");
+    console.log(`pulsado un ${operating}`);
 
-  console.log(`pulsado un ${operating}`);
-
-  switch (operating) {
-    case '∁':
-    //result.innerText = delete.result;
-    break;
-    case '0':
-    case '1':
-    case '2':
-    case '3':
-    case '4':
-    case '5':
-    case '6':
-    case '7':
-    case '8':
-    case '9':
-    
-    
-   // result.innerText = operating + result;
-    break;
-    case '+':
-    case '÷':
-    case '-':
-    case '=':
-    case '×':
-    
-   // result.innerText = operating + result;
-    break;
-    case '∁':
-    break;
-    case '←':
-    break;
-     
+    switch (operating) {
+      case "0":
+      case "1":
+      case "2":
+      case "3":
+      case "4":
+      case "5":
+      case "6":
+      case "7":
+      case "8":
+      case "9":
+        result.innerText = operating;
+        num1 = operating;
+        console.log(`Num1: ${num1}`);
+        break;
+      case "+":
+      case "÷":
+      case "-":
+      case "×":
+        break;
+      case "∁":
+        break;
+      case "←":
+        break;
+      case "=":
+        break;
+    }
   }
-}
 }
